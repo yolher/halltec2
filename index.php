5d6c74b68f64c0e92c8fd2f0601b43f3cc23e0a7
yolher <yocahe5@gmail.com>
2013-05-24 15:28:56 -0700

    primer commit halltec, con logo y divs de paquetes

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>default</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<script src="js/jquery-1.7.2.min.js"></script>
+	<script src="js/jquery.transit.min.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+<header>
+	<div id="logo"><img src="images/logo.png"></div>
+</header>
+
+<section id="paquetes">
+	<div id="planes">plan econimico</div>
+	<div id="planes2">plan econimico</div>
+	<div id="planes3">plan econimico</div>
+	<div id="planes4">plan econimico</div>
+	<div id="planes5">plan econimico</div>
+
+</section>
+
+
+</body>
+</html>
