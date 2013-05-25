d87f479d4a28b3a441d991efd7e34276a6257aa9
yolher <yocahe5@gmail.com>
2013-05-24 17:41:18 -0700

    implementacion de clientes y colores de los paquetes

--- index.php
+++ index.php
@@ -11,16 +11,29 @@
 </head>
 <body>
 <header>
+	<div id="top"></div>
 	<div id="logo"><img src="images/logo.png"></div>
 </header>
 
 <section id="paquetes">
-	<div id="planes">plan econimico</div>
-	<div id="planes2">plan econimico</div>
+	<div id="planes">plan personal</div>
+	<div id="planes2">plan rapido</div>
 	<div id="planes3">plan econimico</div>
-	<div id="planes4">plan econimico</div>
-	<div id="planes5">plan econimico</div>
+	<div id="planes4">plan avanzado</div>
+	<div id="planes5">plan platinum</div>
+	<div id="precio"> $180.000</div>
+	<div id="precio2">$450.000</div>
+	<div id="precio3">$750.000</div>
+	<div id="precio4">$1'150.000</div>
+	<div id="precio5">$1'500.000</div>
+</section>
 
+<section id="clientes">
+<div id="titulo"><span>Nuestros clientes</span></div>
+<div class="paginas"><img src="images/campoalegre.png"></div>
+<div class="paginas"><img src="images/directorio.png"></div>
+<div class="paginas"><img src="images/santacruz.png"></div>
+<div class="paginas"><img src="images/otoniel.png"></div>
 </section>
 
 
